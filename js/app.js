f4d2cf73149d8a4517b27e067a811d799322cc80
Chip C <chipcracraft@gmail.com>
2016-12-16 13:15:57 -0500

    working on sliding function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
-console.log('wired up!')
-console.log($)
-console.log(_)
-console.log(Backbone)
+console.log('wired up!');
+console.log($);
+console.log(_);
+console.log(Backbone);
+
+
+  var aboutMe = document.querySelector(".about-me");
+  var aboutMeHeight = aboutMe.offsetHeight;
+
+$('.arrow').on("click", function(){
+  var effect = 'slide';
+  var options = { direction: "left".val() };
+  var duration = 500;
+  $('.about-me').toggle(effect,options,duration);
+
+});
